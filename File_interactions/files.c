d9586d1e1f36d453f0c2999c637d8bd7574ca64f
Overlizee <73128232+Overlizee@users.noreply.github.com>
2020-11-26 14:33:24 +0100

    updated file.c
    Update of file.c, complete two functions.

--- File_interactions/files.c
+++ File_interactions/files.c
@@ -5,10 +5,58 @@
 
 
 void display_number_of_character(char filename_param[]) {
-
+    int c, number_of_characters = 0;
+    FILE *file_to_read;
+    file_to_read = fopen(filename_param,"r");
+    if(file_to_read == NULL) {
+        printf("ERROR while opening files\n");
+    } else {
+        c = fgetc(file_to_read);
+        while(c != EOF) {
+            number_of_characters += 1;
+            c =  fgetc(file_to_read);
+        }
+        printf("There are %d characters in the text file\n",number_of_characters);
+    }
 }
 
-//function to get the BST/AVL tree of the number of occurences of each character
+//function to get the AVL tree of the number of occurences of each character
 Node** occurence_characters(char filename_param[]){
-   
+    int c;
+    Node **bst_characters=NULL, *temp;
+    FILE *file_to_read;
+    file_to_read = fopen(filename_param,"r");
+
+    if(file_to_read == NULL) {
+        printf("ERROR while opening files\n");
+    } 
+    else {
+        bst_characters = (Node**)malloc(sizeof(Node*));
+        *bst_characters = NULL;
+        c = fgetc(file_to_read);
+        //we check for each letter of the text
+        while(c != EOF) {
+            //we go through the list to see if the character being checked is in it
+            temp = *bst_characters;
+            while( (temp != NULL) && (temp->character != c) ){
+                if(c < temp->character){
+                    temp = temp->left;
+                }
+                else{
+                    temp = temp->right;
+                }
+            }
+            //if not we add it to the list
+            if (temp == NULL){
+                add_BST(bst_characters, 1, c);
+                balance_BST(bst_characters);
+            }
+            //if it is, its number of occurrences increases
+            else{
+                temp->number_of_character++;
+            }
+            c = fgetc(file_to_read);
+        }
+    }
+    return bst_characters;
 }
